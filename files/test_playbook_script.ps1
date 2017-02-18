ab7bb4406fe5c08309586181831329552a0dd4c6
Luke M Griffith <lukemgriffith@gmail.com>
2017-02-18 23:42:16 +0000

    changed.

--- files/test_playbook_script.ps1
+++ files/test_playbook_script.ps1
@@ -1,1 +1,5 @@
-Get-Process
+Write-Host "$env:ComputerName is managed by Ansible."
+Write-Output "$env:ComputerName is managed by Ansible."
+Write-Information "$env:ComputerName is managed by Ansible."
+Write-Verbose "$env:ComputerName is managed by Ansible." -Verbose
+Write-Warning "$env:ComputerName is managed by Ansible." 

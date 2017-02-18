beba1637c17881ca133188f8a362a1978cb57f78
Luke M Griffith <lukemgriffith@gmail.com>
2017-02-18 23:43:25 +0000

    adding stream markers.

--- files/test_playbook_script.ps1
+++ files/test_playbook_script.ps1
@@ -1,5 +1,5 @@
-Write-Host "$env:ComputerName is managed by Ansible."
-Write-Output "$env:ComputerName is managed by Ansible."
-Write-Information "$env:ComputerName is managed by Ansible."
-Write-Verbose "$env:ComputerName is managed by Ansible." -Verbose
-Write-Warning "$env:ComputerName is managed by Ansible." 
+Write-Host "$env:ComputerName is managed by Ansible. host"
+Write-Output "$env:ComputerName is managed by Ansible. output"
+Write-Information "$env:ComputerName is managed by Ansible. info"
+Write-Verbose "$env:ComputerName is managed by Ansible. verbose" -Verbose
+Write-Warning "$env:ComputerName is managed by Ansible. warning" 
